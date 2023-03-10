ed6feb2d433f134f454e53034f09db1dcf074899
Ha0Day <ycis5013@naver.com>
2023-03-10 17:53:21 +0900

    feat(cracking): 1.1 IsUniqe - Draft code

--- cracking/StringManipulation/IsUnique.java
+++ cracking/StringManipulation/IsUnique.java
@@ -3,43 +3,47 @@ package StringManipulation;
 import java.util.HashSet;
 import java.util.Set;
 
+//1.1 Is Unique - 문자열에 중복이 있는지 체크
 public class IsUnique {
     public static void main(String[] args) {
-        String s="adbcfww";
+        String s = "adbcfww";
         System.out.println(checkDuplicates(s));
         System.out.println(checkDuplicates2(s));
         System.out.println(checkDuplicates3(s));
     }
 
-    public static boolean checkDuplicates(String s){
-        char[] charArr=s.toCharArray();
+    //Draft1
+    public static boolean checkDuplicates(String s) {
+        char[] charArr = s.toCharArray();
 
-        for(int i=0;i<charArr.length;i++){
-            int tmp=charArr[i];
-            for(int j=i+1;j<charArr.length;j++){
-                if(tmp==charArr[j]) return true;
+        for (int i = 0; i < charArr.length; i++) {
+            int tmp = charArr[i];
+            for (int j = i + 1; j < charArr.length; j++) {
+                if (tmp == charArr[j]) return true;
             }
         }
         return false;
     }
 
-    public static boolean checkDuplicates2(String s){
-        Set<Character> set= new HashSet<Character>();
+    //Draft2
+    public static boolean checkDuplicates2(String s) {
+        Set<Character> set = new HashSet<Character>();
 
-        for(int i=0;i<s.length();i++){
+        for (int i = 0; i < s.length(); i++) {
             set.add(s.charAt(i));
         }
 
-        if(set.size()==s.length()) return false;
-         return true;
+        if (set.size() == s.length()) return false;
+        return true;
     }
 
-    public static boolean checkDuplicates3(String s){
+    //Draft3
+    public static boolean checkDuplicates3(String s) {
 
-        for(int i=0;i<s.length();i++){
-            int tmp=s.charAt(i);
-            for(int j=i+1;j<s.length();j++){
-                if(tmp==s.charAt(j)) return true;
+        for (int i = 0; i < s.length(); i++) {
+            int tmp = s.charAt(i);
+            for (int j = i + 1; j < s.length(); j++) {
+                if (tmp == s.charAt(j)) return true;
             }
         }
         return false;
