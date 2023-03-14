456fe659336b3c9875321157a897bb6dc4458be9
Ha0Day <ycis5013@naver.com>
2023-03-15 02:01:12 +0900

    feat(cracking) : 1.9 String Rotation - Solution code added

--- cracking/StringManipulation/StringRotation.java
+++ cracking/StringManipulation/StringRotation.java
@@ -10,10 +10,11 @@ import java.util.Arrays;
 //단 isSubString 메서드는 한 번만 호출 가능
 public class StringRotation {
     public static void main(String[] args) {
-        String s1 = "aabbcyaz";
+        String s1 = "aabbcyza";
         String s2 = "bcyzaaab";
 
         System.out.println(isRotatedString(s1, s2));
+        System.out.println(sol_isRotation(s1, s2));
     }
 
     //Draft
@@ -25,5 +26,26 @@ public class StringRotation {
             }
         }
         return false;
+    }
+
+
+    //Solution - O(N)
+    //쪼개지는 부분 기준으로 x, y로 나뉜다고 했을 때
+    //xy=s1, yx=s2
+    //yx는 언제나 xyxy=s1s1의 부분 문자열
+    //isSubstring 함수의 수행 시간에 따라 이 함수의 시간복잡도가 달라짐
+    public static boolean sol_isRotation(String s1, String s2) {
+        int len = s1.length();
+        //s1와 s2의 길이가 같고 빈 문자열이 아닌지 확인
+        if (len == s2.length() && len > 0) {
+            String s1s1 = s1 + s1;
+            return isSubString(s1s1, s2);
+        }
+        return false;
+    }
+
+    //예시로 가져온 코드
+    private static boolean isSubString(String s1s1, String s2) {
+        return s1s1.contains(s2);
     }
 }
