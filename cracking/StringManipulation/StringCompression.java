dfcfb45c86617ca2c165010837845dcefe7ba04e
Ha0Day <ycis5013@naver.com>
2023-03-22 16:12:25 +0900

    feat(cracking) : 1.6 String Compression - Draft code

--- cracking/StringManipulation/StringCompression.java
+++ cracking/StringManipulation/StringCompression.java
@@ -1,2 +1,39 @@
-package StringManipulation;public class StringCompression {
+package StringManipulation;
+
+public class StringCompression {
+    public static void main(String[] args) {
+        System.out.println(compress("aabcccccaaa"));
+        System.out.println(compress("abbbbbc"));
+        System.out.println(compress("abbbc"));
+    }
+
+    //Draft - O(N)
+    public static String compress(String s) {
+
+        //우선 거를 수 있는 것부터 거르는 것이 시간 복잡도에 좋음
+        int num = 1;
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) != s.charAt(i + 1)) {
+                num++;
+            }
+        }
+
+        if (num * 2 >= s.length()) return s;
+
+        StringBuilder sb = new StringBuilder();
+
+        int count = 1;
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                count++;
+                continue;
+            }
+            sb.append(s.charAt(i)).append(count);
+            count = 1;
+        }
+        sb.append(s.charAt(s.length() - 1)).append(count);
+
+        return sb.toString();
+    }
 }
