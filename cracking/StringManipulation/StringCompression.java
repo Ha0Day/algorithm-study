b5405d67d66270bfdea681982204fdf540a99566
Ha0Day <ycis5013@naver.com>
2023-03-22 17:02:02 +0900

    feat(cracking) : 1.6 String Compression - Draft code updated, Solution code added

--- cracking/StringManipulation/StringCompression.java
+++ cracking/StringManipulation/StringCompression.java
@@ -2,23 +2,32 @@ package StringManipulation;
 
 public class StringCompression {
     public static void main(String[] args) {
-        System.out.println(compress("aabcccccaaa"));
+        System.out.println(compress("aabcccccaaaaaaaaaaaaaaa"));
         System.out.println(compress("abbbbbc"));
         System.out.println(compress("abbbc"));
+
+        System.out.println();
+
+        System.out.println(sol_compress("aabcccccaaaaaaaaaaaaaaa"));
+        System.out.println(sol_compress("abbbbbc"));
+        System.out.println(sol_compress("abbbc"));
     }
 
     //Draft - O(N)
     public static String compress(String s) {
 
-        //우선 거를 수 있는 것부터 거르는 것이 시간 복잡도에 좋음
-        int num = 1;
+        //우선 거를 수 있는 것부터 거르기
+        int num = 0;
+        int compressedlength = 0;
         for (int i = 0; i < s.length() - 1; i++) {
+            num++;
             if (s.charAt(i) != s.charAt(i + 1)) {
-                num++;
+                compressedlength += 1 + String.valueOf(num).length();
+                num = 0;
             }
         }
 
-        if (num * 2 >= s.length()) return s;
+        if (compressedlength >= s.length()) return s;
 
         StringBuilder sb = new StringBuilder();
 
@@ -35,5 +44,40 @@ public class StringCompression {
         sb.append(s.charAt(s.length() - 1)).append(count);
 
         return sb.toString();
+    }
+
+    //Solution - O(N)
+    private static String sol_compress(String str) {
+        int finalLength = countCompression(str);
+        if (finalLength >= str.length()) return str;
+
+        StringBuilder compressed = new StringBuilder(finalLength);
+        int countConsecutive = 0;
+        for (int i = 0; i < str.length(); i++) {
+            countConsecutive++;
+
+            //다음 문자와 현재 문자가 같지 않다면 현재 문자를 결과 문자열에 추가한다.
+            if (i + 1 >= str.length() || str.charAt(i) != str.charAt(i + 1)) {
+                compressed.append(str.charAt(i));
+                compressed.append(countConsecutive);
+                countConsecutive = 0;
+            }
+        }
+        return compressed.toString();
+    }
+
+    private static int countCompression(String str) {
+        int compressedLength = 0;
+        int countConsecutive = 0;
+        for (int i = 0; i < str.length(); i++) {
+            countConsecutive++;
+
+            //다음 문자와 현재 문자가 같지 않다면 길이를 증가시킨다.
+            if (i + 1 >= str.length() || str.charAt(i) != str.charAt(i + 1)) {
+                compressedLength += 1 + String.valueOf(countConsecutive).length();
+                countConsecutive = 0;
+            }
+        }
+        return compressedLength;
     }
 }
