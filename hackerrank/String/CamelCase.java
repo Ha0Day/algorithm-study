69cd7a1c049ac447c2fe1dd99ff3b4212353155f
Ha0Day <ycis5013@naver.com>
2023-03-22 19:36:45 +0900

    feat(hackerrank) : Calmel Case - solved

--- hackerrank/String/CamelCase.java
+++ hackerrank/String/CamelCase.java
@@ -1,2 +1,22 @@
-package String;public class CamelCase {
+package String;
+
+import java.io.IOException;
+
+public class CamelCase {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(camelcase("iAmHaYoung"));
+    }
+
+    public static int camelcase(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch >= 'A' && ch <= 'Z') {
+                count++;
+            }
+        }
+        return count + 1;
+    }
+
 }
