92631e3868b44cb4d3650be18141a406d3b7e210
Ha0Day <ycis5013@naver.com>
2023-03-22 19:31:51 +0900

    feat(cracking) : 1.5 URLify - Solution code added

--- cracking/StringManipulation/URLify.java
+++ cracking/StringManipulation/URLify.java
@@ -11,6 +11,10 @@ public class URLify {
         System.out.println(urlify(s, len));
         System.out.println(urlify2(s, len));
 
+        System.out.println(s);
+        sol_replaceSpaces(s, len);
+        System.out.println(s);
+
     }
 
     //Draft - O(N)
@@ -33,4 +37,31 @@ public class URLify {
         return s.replaceAll(" ", "%20");
     }
 
+
+    //Solution - O(N)
+    private static void sol_replaceSpaces(String s, int trueLength) {
+
+        char[] str = s.toCharArray();
+
+        int spaceCount = 0, index, i;
+        for (i = 0; i < trueLength; i++) {
+            if (str[i] == ' ') {
+                spaceCount++;
+            }
+        }
+
+        index = trueLength + spaceCount * 2;
+
+        for (i = trueLength - 1; i >= 0; i--) {
+            if (str[i] == ' ') {
+                str[index - 1] = '0';
+                str[index - 2] = '2';
+                str[index - 3] = '%';
+                index = index - 3;
+            } else {
+                str[index - 1] = str[i];
+                index--;
+            }
+        }
+    }
 }
