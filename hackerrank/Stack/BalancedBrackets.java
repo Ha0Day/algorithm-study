83fe162687fd3078514ea2456089b0ce1df260e3
Ha0Day <ycis5013@naver.com>
2023-03-29 22:02:59 +0900

    feat(hackerrank) : Balanced Brackets - Draft code refactored, Another solution code added

--- hackerrank/Stack/BalancedBrackets.java
+++ hackerrank/Stack/BalancedBrackets.java
@@ -8,38 +8,62 @@ public class BalancedBrackets {
     }
 
     //Draft code
+    //Refactored
     public static String checkBrackets(String expression) {
         if (expression.length() % 2 != 0) return "NO";
 
         Stack<Character> stack = new Stack();
 
         for (int i = 0; i < expression.length(); i++) {
-            if (expression.charAt(i) != ']' && expression.charAt(i) != ')' && expression.charAt(i) != '}') {
-                stack.push(expression.charAt(i));
-                continue;
+            char c = expression.charAt(i);
+            if (c != ']' && c != ')' && c != '}') {
+                stack.push(c);
             }
 
-            if(stack.isEmpty()) return "NO";
+            if (stack.isEmpty()) return "NO";
 
-            if (expression.charAt(i) == ']') {
-                if (stack.peek() == '[') {
-                    stack.pop();
-                }
+            char top = stack.peek();
+            if (c == ']' && top == '[') {
+                stack.pop();
             }
-            if (expression.charAt(i) == ')') {
-                if (stack.peek() == '(') {
-                    stack.pop();
-                }
+            if (c == ')' && top == '(') {
+                stack.pop();
             }
-            if (expression.charAt(i) == '}') {
-                if (stack.peek() == '{') {
+            if (c == '}' && top == '{') {
+                stack.pop();
+            }
+        }
+
+        if (stack.isEmpty()) return "YES";
+
+        return "NO";
+    }
+
+    //Another Solution
+    public static String checkBrackets2(String expression){
+        Stack<Character> stack = new Stack();
+
+        for(char c : expression.toCharArray())
+        {
+            if(c == '(')
+                stack.push(')');
+            else if(c == '{')
+                stack.push('}');
+            else if(c == '[')
+                stack.push(']');
+
+            else{
+                if( stack.isEmpty() || c != stack.peek()){
+                    return "NO";
+                }
+                else{
                     stack.pop();
                 }
             }
         }
-
-        if(stack.isEmpty()) return "YES";
-
-        return "NO";
+        if(stack.isEmpty())
+            return "YES";
+        else
+            return "NO";
     }
 }
