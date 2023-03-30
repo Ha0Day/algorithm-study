3989e7a879f96e74c2044e93912aed55249e29fa
Ha0Day <ycis5013@naver.com>
2023-03-30 12:54:49 +0900

    feat(cracking) : 3.3 Stack of Plates - Solution code added

--- cracking/Stacks_Queues/StackOfPlates.java
+++ cracking/Stacks_Queues/StackOfPlates.java
@@ -1,6 +1,8 @@
 package Stacks_Queues;
 
+import java.sql.Array;
 import java.util.ArrayList;
+import java.util.EmptyStackException;
 import java.util.List;
 import java.util.Stack;
 
@@ -15,62 +17,66 @@ public class StackOfPlates {
     public static void main(String[] args) {
 
         //for Draft code
-        SetOfStacks stacks = new SetOfStacks(3);
+        //SetOfStacks stacks = new SetOfStacks(3);
+
+        //for Solution code
+        sol_SetOfStacks stacks = new sol_SetOfStacks(3);
+
 
         stacks.push(1);
-        System.out.println("push 1");
+        /*System.out.println("push 1");
         System.out.println("current index = " + stacks.indexOfAllStacks);
-        System.out.println("current stack = " + stacks.indexOfLastStack());
+        System.out.println("current stack = " + stacks.indexOfLastStack());*/
 
         stacks.push(2);
-        System.out.println("push 2");
+        /*System.out.println("push 2");
         System.out.println("current index = " + stacks.indexOfAllStacks);
-        System.out.println("current stack = " + stacks.indexOfLastStack());
+        System.out.println("current stack = " + stacks.indexOfLastStack());*/
 
         stacks.push(3);
-        System.out.println("push 3");
+        /*System.out.println("push 3");
         System.out.println("current index = " + stacks.indexOfAllStacks);
-        System.out.println("current stack = " + stacks.indexOfLastStack());
+        System.out.println("current stack = " + stacks.indexOfLastStack());*/
 
         stacks.push(4);
-        System.out.println("push 4");
+        /*System.out.println("push 4");
         System.out.println("current index = " + stacks.indexOfAllStacks);
-        System.out.println("current stack = " + stacks.indexOfLastStack());
+        System.out.println("current stack = " + stacks.indexOfLastStack());*/
 
         stacks.push(5);
-        System.out.println("push 5");
+        /*System.out.println("push 5");
         System.out.println("current index = " + stacks.indexOfAllStacks);
-        System.out.println("current stack = " + stacks.indexOfLastStack());
+        System.out.println("current stack = " + stacks.indexOfLastStack());*/
 
         stacks.push(6);
-        System.out.println("push 6");
+        /*System.out.println("push 6");
         System.out.println("current index = " + stacks.indexOfAllStacks);
-        System.out.println("current stack = " + stacks.indexOfLastStack());
+        System.out.println("current stack = " + stacks.indexOfLastStack());*/
 
         stacks.push(7);
-        System.out.println("push 7");
+        /*System.out.println("push 7");
         System.out.println("current index = " + stacks.indexOfAllStacks);
-        System.out.println("current stack = " + stacks.indexOfLastStack());
+        System.out.println("current stack = " + stacks.indexOfLastStack());*/
 
         stacks.push(8);
-        System.out.println("push 8");
+        /*System.out.println("push 8");
         System.out.println("current index = " + stacks.indexOfAllStacks);
-        System.out.println("current stack = " + stacks.indexOfLastStack());
+        System.out.println("current stack = " + stacks.indexOfLastStack());*/
 
         stacks.push(9);
-        System.out.println("push 8");
+        /*System.out.println("push 8");
         System.out.println("current index = " + stacks.indexOfAllStacks);
-        System.out.println("current stack = " + stacks.indexOfLastStack());
+        System.out.println("current stack = " + stacks.indexOfLastStack());*/
 
         stacks.pop();
-        System.out.println("pop");
+        /*System.out.println("pop");
         System.out.println("current index = " + stacks.indexOfAllStacks);
-        System.out.println("current stack = " + stacks.indexOfLastStack());
+        System.out.println("current stack = " + stacks.indexOfLastStack());*/
 
         stacks.popAt(1);
-        System.out.println("popAt 1");
+        /*System.out.println("popAt 1");
         System.out.println("current index = " + stacks.indexOfAllStacks);
-        System.out.println("current stack = " + stacks.indexOfLastStack());
+        System.out.println("current stack = " + stacks.indexOfLastStack());*/
 
         stacks.printAllStacks();
 
@@ -155,6 +161,135 @@ public class StackOfPlates {
             }
 
             return arr[arrSize - 1];
+        }
+    }
+
+
+    //Solution code
+    //stack을 linkedlist로 구현하여
+    //popAt 함수의 경우 shift할 때 시간 및 공간 복잡도가 조금 더 나을 것 같음
+    private static class sol_SetOfStacks {
+        ArrayList<MyStack> stacks = new ArrayList();
+        public int capacity;
+
+        public sol_SetOfStacks(int capacity) {
+            this.capacity = capacity;
+        }
+
+        void printAllStacks() {
+            for (int i = 0; i < stacks.size(); i++) {
+                while (!stacks.get(i).isEmpty()) {
+                    System.out.print(stacks.get(i).pop() + " ");
+                }
+                System.out.println();
+            }
+        }
+
+        public MyStack getLastStack() {
+            if (stacks.size() == 0) return null;
+            return stacks.get(stacks.size() - 1);
+        }
+
+        public void push(int v) {
+            MyStack last = getLastStack();
+            if (last != null && !last.isFull()) { //마지막 스택에 추가한다.
+                last.push(v);
+            } else {  //스택을 새로 만든다.
+                MyStack stack = new MyStack(capacity);
+                stack.push(v);
+                stacks.add(stack);
+            }
+        }
+
+        public int pop() {
+            MyStack last = getLastStack();
+            if (last == null) throw new EmptyStackException();
+            int v = last.pop();
+            if (last.size == 0) stacks.remove(stacks.size() - 1);
+            return v;
+        }
+
+        public boolean isEmpty() {
+            MyStack last = getLastStack();
+            return last == null || last.isEmpty();
+        }
+
+        public int popAt(int index) {
+            return leftShift(index, true);
+        }
+
+        public int leftShift(int index, boolean removeTop) {
+            MyStack stack = stacks.get(index);
+            int removed_item;
+            //제거할 item을 pop
+            if (removeTop) removed_item = stack.pop();
+                //item 제거 후 뒤 스택의 가장 아래 데이터 제거
+            else removed_item = stack.removeBottom();
+            if (stack.isEmpty()) {
+                stacks.remove(index);
+            } else if (stacks.size() > index + 1) { //pop된 스택의 바로 뒤 스택부터 마지막 스택까지
+                int v = leftShift(index + 1, false); //재귀
+                //앞 스택에 뒤 스택의 가장 아래 데이터를 push
+                stack.push(v);
+            }
+            return removed_item;
+        }
+    }
+
+    public static class MyStack {
+        private int capacity;
+        public Node top, bottom;
+        public int size = 0;
+
+        public MyStack(int capacity) {
+            this.capacity = capacity;
+        }
+
+        public boolean isFull() {
+            return capacity == size;
+        }
+
+        public void join(Node above, Node below) {
+            if (below != null) below.above = above;
+            if (above != null) above.below = below;
+        }
+
+        public boolean push(int v) {
+            if (size >= capacity) return false;
+            size++;
+            Node n = new Node(v);
+            if (size == 1) bottom = n;
+            join(n, top);
+            top = n;
+            return true;
+        }
+
+        public int pop() {
+            Node t = top;
+            top = top.below;
+            size--;
+            return t.value;
+        }
+
+        public boolean isEmpty() {
+            return size == 0;
+        }
+
+        public int removeBottom() {
+            Node b = bottom;
+            bottom = bottom.above;
+            if (bottom != null) bottom.below = null;
+            size--;
+            return b.value;
+        }
+    }
+
+    public static class Node {
+        int value;
+        Node below, above;
+
+        Node(int value) {
+            this.value = value;
         }
     }
 }
