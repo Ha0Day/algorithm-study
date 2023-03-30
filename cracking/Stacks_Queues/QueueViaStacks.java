39c55cb9d6df6c9f91591c85a7557edae64b345b
Ha0Day <ycis5013@naver.com>
2023-03-30 14:59:59 +0900

    feat(cracking) : 3.4 Queue via Stacks - Solution code added

--- cracking/Stacks_Queues/QueueViaStacks.java
+++ cracking/Stacks_Queues/QueueViaStacks.java
@@ -6,26 +6,20 @@ import java.util.Stack;
 //스택 두 개로 큐 하나를 구현한 MyQueue 클래스를 구현하라
 public class QueueViaStacks {
     public static void main(String[] args) {
-        MyQueue queue = new MyQueue();
+
+        //MyQueue queue = new MyQueue();    //for Draft code
+        sol_MyQueue queue = new sol_MyQueue();  //for Solution code
 
         queue.enqueue(1);
-        queue.printQueue();
         queue.enqueue(2);
-        queue.printQueue();
         queue.enqueue(3);
-        queue.printQueue();
         queue.dequeue();
-        queue.printQueue();
         queue.enqueue(4);
-        queue.printQueue();
+        System.out.println("peek: " + queue.peek());
         queue.dequeue();
-        queue.printQueue();
-        System.out.println("peek: "+queue.peek());
         queue.dequeue();
-        queue.printQueue();
-        System.out.println(queue.isEmpty());
+        System.out.println("peek: " + queue.peek());
         queue.dequeue();
-        queue.printQueue();
         System.out.println(queue.isEmpty());
 
     }
@@ -80,6 +74,55 @@ public class QueueViaStacks {
 
         boolean isEmpty() {
             return stack.isEmpty();
+        }
+    }
+
+
+    //Solution code
+    //Draft code와 아이디어는 동일하나
+    //Draft code의 경우 pop/peek 연산이 연이어 호출될 때 쓸데없이 원소를 오락가락하게 옮기게 됨
+    //여기서는 원소들의 순서를 반드시 뒤집어야 할 경우가 아니면 두 번째 스택에 그대로 두는 게으른(lazy) 접근법을 사용
+    //stack
+    private static class sol_MyQueue {
+        Stack<Integer> stackNewest; //새 원소가 맨 위에 있는 스택
+        Stack<Integer> stackOldest; //마지막 원소가 맨 위에 있는 스택
+
+        //stackOldest가 비어 있을 때만 stackNewest의 모든 원소의 순서를 뒤집어서 stackOldest에 넣음
+        //원소 삭제는 stackOldest에서, 원소 추가는 stackNewest에 함
+
+        sol_MyQueue() {
+            stackNewest = new Stack<>();
+            stackOldest = new Stack<>();
+        }
+
+        public int size() {
+            return stackNewest.size() + stackOldest.size();
+        }
+
+        void enqueue(int item) {
+            stackNewest.push(item);
+        }
+
+        private void shiftStacks() {
+            if (stackOldest.isEmpty()) {
+                while (!stackNewest.isEmpty()) {
+                    stackOldest.push(stackNewest.pop());
+                }
+            }
+        }
+
+        int peek() {
+            shiftStacks();
+            return stackOldest.peek();
+        }
+
+        int dequeue() {
+            shiftStacks();
+            return stackOldest.pop();
+        }
+
+        boolean isEmpty() {
+            return stackOldest.isEmpty() && stackNewest.isEmpty();
         }
     }
 }
