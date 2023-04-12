7e6a975c9d65c96b83b66d17c7ddcc19e230a3cd
Ha0Day <ycis5013@naver.com>
2023-04-12 15:20:20 +0900

    feat(cracking) : 2.7 Intersection - Solution code added

--- cracking/LinkedList/Intersection.java
+++ cracking/LinkedList/Intersection.java
@@ -19,8 +19,6 @@ public class Intersection {
         Node bnode1 = new Node(1);
         Node bnode2 = anode4;
         Node bnode3 = new Node(5);
-        Node bnode4 = new Node(4);
-        Node bnode5 = new Node(4);
 
         System.out.println(bnode2);
 
@@ -30,11 +28,9 @@ public class Intersection {
         anode4.next = anode5;
 
         bnode1.next = bnode2;
-        bnode2.next = bnode3;
-        bnode3.next = bnode4;
-        bnode4.next = bnode5;
+        //bnode2.next = bnode3;   //anode4(bnode2)와 anode5의 연결이 끊기게 됨
 
-        System.out.println(intersection(anode1, bnode1));
+        System.out.println(sol_findIntersection(anode1, bnode1));
     }
 
     //Draft code
@@ -46,7 +42,7 @@ public class Intersection {
         while (anode != null) {
             bnode = tmp;
             while (bnode != null) {
-                if (anode.equals(bnode)) {
+                if (anode.equals(bnode)) {  //equals 대신 == 사용 가능
                     return anode;
                 }
                 bnode = bnode.next;
@@ -54,5 +50,62 @@ public class Intersection {
             anode = anode.next;
         }
         return null;
+    }
+
+    //살짝 별개의 내용이지만 교집합의 존재 유무는 단순히 두 리스트의 마지막 노드가 같은지만 보면 알 수 있다
+    //교집합이 생긴 순간 이후부터는 같은 길을 가기 때문
+
+    //Solution
+    /*
+    1. 각 연결리스트를 순회하면서 길이와 마지막 노드를 구한다.
+    2. 마지막 노드를 비교한 뒤, 그 참조값이 다르다면 교집합이 없다는 사실을 바로 반환한다.
+    3. 두 연결리스트의 시작점에 포인터를 놓는다.
+    4. 길이가 더 긴 리스트의 포인터를 두 길이의 차이만큼 앞으로 움직인다.
+    5. 두 포인터가 같아질 때까지 두 리스트를 함께 순회한다.
+    */
+    //시간복잡도 O(N)
+
+    static int lengthOfList(Node node) {
+        int size = 0;
+        while (node != null) {
+            size++;
+            node = node.next;
+        }
+        return size;
+    }
+
+    static Node lastNodeOfList(Node node) {
+        while (node.next != null) {
+            node = node.next;
+        }
+        return node;
+    }
+
+    static Node sol_findIntersection(Node anode, Node bnode) {
+        if (lastNodeOfList(anode) != lastNodeOfList(bnode)) {
+            return null;
+        }
+
+        int dist;
+        Node apoint = anode;
+        Node bpoint = bnode;
+
+        if (lengthOfList(anode) > lengthOfList(bnode)) {
+            dist = lengthOfList(anode) - lengthOfList(bnode);
+            for (int i = 0; i < dist; i++) {
+                apoint = apoint.next;
+            }
+        } else {
+            dist = lengthOfList(bnode) - lengthOfList(anode);
+            for (int i = 0; i < dist; i++) {
+                bpoint = bpoint.next;
+            }
+        }
+
+        while (apoint != bpoint) {
+            apoint = apoint.next;
+            bpoint = bpoint.next;
+        }
+        return apoint;
     }
 }
