27227ea1a535c2e01fa664ee258191de5028c4d7
Ha0Day <ycis5013@naver.com>
2023-04-15 14:40:07 +0900

    feat(cracking) : Node class for Trees and Graphs

---
+++ cracking/Trees_Graphs/Node.java
@@ -0,0 +1,13 @@
+package Trees_Graphs;
+
+public class Node {
+    int data;
+    int level;
+    Node left;
+    Node right;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+    }
+}
