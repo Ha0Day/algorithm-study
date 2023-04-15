a28e9f351318b548178ed5140be4e1dd236ed964
Ha0Day <ycis5013@naver.com>
2023-04-15 15:01:00 +0900

    feat(cracking) : 2.3 Delete Middle Node - Node class for LinkedList modified

--- cracking/LinkedList/Node.java
+++ cracking/LinkedList/Node.java
@@ -3,9 +3,17 @@ package LinkedList;
 public class Node {
     public int data;
     public Node next;
+    public boolean valid;
+
+    public Node(){
+        //this.data = -1;
+        //this.next = null;
+        //this.valid = true;
+    }
 
     public Node(int data) {
         this.data = data;
         this.next = null;
+        this.valid = true;
     }
 }
