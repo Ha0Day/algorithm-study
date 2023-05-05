3a3b4e7ed63d8fd4d0360b234f85e7610bf1d398
Ha0Day <ycis5013@naver.com>
2023-05-05 21:11:52 +0900

    refactor(baekjoon) : 9663번 - Refactored

--- baekjoon/BackTracking/Q9663.java
+++ baekjoon/BackTracking/Q9663.java
@@ -7,44 +7,43 @@ import java.io.InputStreamReader;
 public class Q9663 {
     static int N;
     static int count;
-    static int[][] table;
+    static int[] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         N = Integer.parseInt(br.readLine());
-        table = new int[N][N];
+        arr = new int[N];   //인덱스가 열, 값이 행
         count = 0;
+
         nQueen(0);
         System.out.print(count);
     }
 
-    private static void nQueen(int row) {
-        if (row == N) {
-            count = count + 1;
+    private static void nQueen(int col) {
+        if (col == N) {
+            count++;
             return;
         }
 
         for (int i = 0; i < N; i++) {
-            if (!check(row, i)) {
-                continue;
+            arr[col] = i;
+            if (check(col)) {
+                nQueen(col + 1);
             }
-            table[row][i] = 1;
-            nQueen(row + 1);
-            table[row][i] = 0;
         }
     }
 
-    static boolean check(int r, int c) {
-        for (int i = 0; i < N; i++) {
-            if (table[i][c] == 1) return false;
-        }
-        int i, j;
-        for (i = r - 1, j = c - 1; i >= 0 && j >= 0; i--, j--) {
-            if (table[i][j] == 1) return false;
-        }
-        for (i = r - 1, j = c + 1; i >= 0 && j < N; i--, j++) {
-            if (table[i][j] == 1) return false;
+    static boolean check(int col) {
+        for (int i = 0; i < col; i++) {
+            //같은 행에 위치한 경우
+            if (arr[i] == arr[col]){
+                return false;
+            }
+            //대각선에 놓여 있는 경우
+            // 열의 차와 행의 차가 같을 경우가 대각선에 놓여있는 경우다
+            else if (Math.abs(i - col) == Math.abs(arr[i] - arr[col])){
+                return false;
+            }
         }
         return true;
     }
