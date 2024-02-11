171f12699c7cd17c7daba43ee1e5ad5a9236acc8
전하영 <ycis5013@naver.com>
2024-02-11 21:54:33 +0900

    fix(boj): 2223번 금화 - 조건 추가

--- baekjoon/Implementation/BOJ_S3_2223_금화.java
+++ baekjoon/Implementation/BOJ_S3_2223_금화.java
@@ -10,32 +10,36 @@ public class BOJ_S3_2223_금화 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int min = Integer.MAX_VALUE;
-        int result = 0;
+        int result = 0; //정답
 
         int t = Integer.parseInt(st.nextToken());
         int x = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        int d[] = new int[m];
-        int s[] = new int[m];
-        int left[] = new int[m];
 
-
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            d[i] = Integer.parseInt(st.nextToken());
-            s[i] = Integer.parseInt(st.nextToken());
-            if ((d[i] - 1) / s[i] < min) {
-                min = (d[i] - 1) / s[i];
-            }
-        }
-        if (min == 0) {
-            result = 0;
-        } else if (t < min) {
+        if (m == 0) { //몬스터가 없는 경우
             result = t * x;
         } else {
-            result = min * x;
-            t = t - min;
-            result += (t / 2) * x;
+            int d[] = new int[m];
+            int s[] = new int[m];
+
+
+            for (int i = 0; i < m; i++) {
+                st = new StringTokenizer(br.readLine());
+                d[i] = Integer.parseInt(st.nextToken());
+                s[i] = Integer.parseInt(st.nextToken());
+                if ((d[i] - 1) / s[i] < min) {
+                    min = (d[i] - 1) / s[i]; //가장 빨리 도착하는 몬스터가 걸리는 시간 - 1
+                }
+            }
+            if (min == 0) {
+                result = 0;
+            } else if (t < min) {
+                result = t * x;
+            } else {
+                result = min * x;
+                t = t - min;
+                result += (t / 2) * x; //남은 시간은 멈추고 줍기를 1시간 마다 반복
+            }
         }
         System.out.println(result);
     }
