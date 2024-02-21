d2d96a391342ccf374585a6a355a9fa3baa1f32d
전하영 <ycis5013@naver.com>
2024-02-21 23:51:05 +0900

    feat(boj): 11501번 S2 주식

---
+++ baekjoon/BOJ_S2_11501_주식.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_S2_11501_주식 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int t = 1; t <= T; t++) {
+            long profit = 0; //이익
+            int days = Integer.parseInt(br.readLine());
+            int[] price = new int[days]; //날 별 주가
+            int max = Integer.MIN_VALUE; //현재 최대 주가
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < days; i++) {
+                price[i] = Integer.parseInt(st.nextToken()); //입력 받기
+            }
+            for (int i = days - 1; i >= 0; i--) { //뒤에서부터 확인
+                if (price[i] > max) {
+                    max = price[i]; //최댓값 갱신
+                } else {
+                    profit += (max - price[i]);
+                }
+            }
+            System.out.println(profit);
+        }
+    }
+}
